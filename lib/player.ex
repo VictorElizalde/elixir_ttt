2b90bc152d7132a029c431c6cb06b38f6aa0f139
Victor Elizalde <vic-bull@hotmail.com>
2021-02-11 15:38:12 -0600

    Applies identation and changes marker to token

--- lib/player.ex
+++ lib/player.ex
@@ -1,46 +1,46 @@
 defmodule Player do
-    @human_player "human"
-    @computer_player "computer"
+  @human_player "human"
+  @computer_player "computer"
 
-    defprotocol(PlayerInstruction, do: def(get_instruction(player)))
+  defprotocol(PlayerInstruction, do: def(get_instruction(player)))
 
-    defimpl PlayerInstruction, for: HumanPlayer do
-        def get_instruction(_human_player) do
-            "Select a position between 1 and 9"
-        end
+  defimpl PlayerInstruction, for: HumanPlayer do
+    def get_instruction(_human_player) do
+      "Select a position between 1 and 9"
     end
+  end
 
-    defimpl PlayerInstruction, for: ComputerPlayer do
-        def get_instruction(_computer_player) do
-            "Computer's turn"
-        end
+  defimpl PlayerInstruction, for: ComputerPlayer do
+    def get_instruction(_computer_player) do
+      "Computer's turn"
     end
+  end
 
-    defprotocol PlayerInvalid do
-        @fallback_to_any true
-        def get_invalid_instruction(player)
-      end
+  defprotocol PlayerInvalid do
+    @fallback_to_any true
+    def get_invalid_instruction(player)
+  end
 
-    defimpl PlayerInvalid, for: Any do
-        def get_invalid_instruction(_) do
-          "Invalid move, try again"
-        end
+  defimpl PlayerInvalid, for: Any do
+    def get_invalid_instruction(_) do
+      "Invalid move, try again"
     end
+  end
 
-    def create_player(marker, type) do
-        case type do
-        @human_player ->
-            create_human_player(marker)
-        @computer_player ->
-            create_computer_player(marker)
-        end
+  def create_player(token, type) do
+    case type do
+    @human_player ->
+      create_human_player(token)
+    @computer_player ->
+      create_computer_player(token)
     end
+  end
 
-    def create_human_player(marker) do
-        %HumanPlayer{marker: marker}
-    end
+  def create_human_player(token) do
+    %HumanPlayer{token: token}
+  end
 
-    def create_computer_player(marker) do
-        %ComputerPlayer{marker: marker}
-    end
+  def create_computer_player(token) do
+    %ComputerPlayer{token: token}
+  end
 end
--- test/ui_test.exs
+++ test/ui_test.exs
@@ -1,72 +1,72 @@
 defmodule UiTest do
-    use ExUnit.Case, async: true
-    import Ui
-    import Player
-    import HumanPlayer
-    import ComputerPlayer
-    import ExUnit.CaptureIO
+  use ExUnit.Case, async: true
+  import Ui
+  import Player
+  import HumanPlayer
+  import ComputerPlayer
+  import ExUnit.CaptureIO
 
-    defprotocol(PlayerInstruction, do: def(get_instruction(player)))
+  defprotocol(PlayerInstruction, do: def(get_instruction(player)))
 
-    defimpl PlayerInstruction, for: HumanPlayer do
-        def get_instruction(_human_player) do
-            "Select a position between 1 and 9"
-        end
+  defimpl PlayerInstruction, for: HumanPlayer do
+    def get_instruction(_human_player) do
+      "Select a position between 1 and 9"
     end
+  end
 
-    defimpl PlayerInstruction, for: ComputerPlayer do
-        def get_instruction(_computer_player) do
-            "Computer's turn"
-        end
+  defimpl PlayerInstruction, for: ComputerPlayer do
+    def get_instruction(_computer_player) do
+      "Computer's turn"
     end
+  end
 
-    defprotocol PlayerInvalid do
-      @fallback_to_any true
-      def get_invalid_instruction(player)
+  defprotocol PlayerInvalid do
+    @fallback_to_any true
+    def get_invalid_instruction(player)
+  end
+
+  defimpl PlayerInvalid, for: Any do
+    def get_invalid_instruction(_) do
+      "Invalid move, try again"
     end
+  end
 
-    defimpl PlayerInvalid, for: Any do
-        def get_invalid_instruction(_) do
-          "Invalid move, try again"
-        end
-    end
+  @human  create_player("X", "human")
+  @computer create_player("O", "computer")
 
-    @human  create_player("X", "human")
-    @computer create_player("O", "computer")
+  test "prints 3x3 board" do
+    assert capture_io(fn ->
+      print_board([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    end) == "| 1 | 2 | 3 |\n| 4 | 5 | 6 |\n| 7 | 8 | 9 |\n"
+  end
 
-    test "prints 3x3 board" do
-        assert capture_io(fn ->
-            print_board([1, 2, 3, 4, 5, 6, 7, 8, 9])
-        end) == "| 1 | 2 | 3 |\n| 4 | 5 | 6 |\n| 7 | 8 | 9 |\n"
-    end
+  test "prints user instructions to make a move" do
+    assert capture_io(fn ->
+      print_player_instructions(PlayerInstruction.get_instruction(@human))
+    end) == "Select a position between 1 and 9\n" 
+  end
 
-    test "prints user instructions to make a move" do
-        assert capture_io(fn ->
-          print_player_instructions(PlayerInstruction.get_instruction(@human))
-        end) == "Select a position between 1 and 9\n" 
-    end
+  test "prints computer instructions to make a move" do
+    assert capture_io(fn ->
+      print_player_instructions(PlayerInstruction.get_instruction(@computer))
+    end) == "Computer's turn\n" 
+  end
 
-    test "prints computer instructions to make a move" do
-        assert capture_io(fn ->
-          print_player_instructions(PlayerInstruction.get_instruction(@computer))
-        end) == "Computer's turn\n" 
-    end
+  test "prints invalid move" do
+    assert capture_io(fn ->
+      print_invalid_player_move(PlayerInvalid.get_invalid_instruction(@human))
+    end) == "Invalid move, try again\n"
+  end
 
-    test "prints invalid move" do
-        assert capture_io(fn ->
-          print_invalid_player_move(PlayerInvalid.get_invalid_instruction(@human))
-        end) == "Invalid move, try again\n"
-    end
+  test "prints tie" do
+    assert capture_io(fn ->
+      print_tie()
+    end) == "Tie!\n"
+  end
 
-    test "prints tie" do
-        assert capture_io(fn ->
-          print_tie()
-        end) == "Tie!\n"
-    end
-
-    test "prints winner" do
-        assert capture_io(fn ->
-          print_winner("X")
-        end) == "Winner is X!\n"
-    end
+  test "prints winner" do
+    assert capture_io(fn ->
+      print_winner("X")
+    end) == "Winner is X!\n"
+  end
 end
